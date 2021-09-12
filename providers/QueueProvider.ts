936062690044a06e1e3de29e19481fc4eec0486d
Harvey Hoang <harvey.h@webprovise.com>
2021-09-12 10:48:03 +0700

    Update v1.0.0

--- providers/QueueProvider.ts
+++ providers/QueueProvider.ts
@@ -7,23 +7,31 @@
  * file that was distributed with this source code.
  */
 
-import { IocContract } from '@adonisjs/fold'
+// import { IocContract } from '@adonisjs/fold'
 import QueueManager from '../src/queue/QueueManager'
-import { Application } from '@adonisjs/application'
+// import { Application } from '@adonisjs/application'
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+// import Config from '@ioc:Adonis/Core/Config'
 /**
  * Provider to bind redis to the container
  */
 export default class CommandProvider {
-	constructor(protected container: IocContract) {}
+	constructor(protected app: ApplicationContract) {}
+	// constructor(protected container: IocContract) {}
 
 	/**
 	 * Register the redis binding
 	 */
 	public register() {
-		this.container.singleton('Adonis5/Queue', () => {
-			const app: Application = this.container.use('Adonis/Core/Application')
-			const queueConfig = this.container.use('Adonis/Core/Config').get('queue', {})
-			return new QueueManager(queueConfig, app.appRoot)
+		// this.container.singleton('Adonis5/Queue', () => {
+		// 	const app: Application = this.container.use('Adonis/Core/Application')
+		// 	const queueConfig = this.container.use('Adonis/Core/Config').get('queue', {})
+		// 	return new QueueManager(queueConfig, app.appRoot)
+		// })
+		const queueConfig = this.app.container.use('Adonis/Core/Config').get('queue', {})
+		const appRoot = this.app.appRoot
+		this.app.container.singleton('Adonis5/Queue', () => {
+			return new QueueManager(queueConfig, appRoot)
 		})
 	}
 
